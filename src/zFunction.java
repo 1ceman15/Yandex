0750abda8e19c748777957dfa518af3c226a3948
Muradly Muhammed <mmuradly2@gmail.com>
2024-01-31 22:08:35 +0300

    Aded zFunction with binary search

--- src/zFunction.java
+++ src/zFunction.java
@@ -1,15 +1,11 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class zFunction {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         String str = br.readLine();
-        if (str.length() == 1) {
-            System.out.println(0);
-            return;
-        }
+
         long[] array = new long[str.length()];
         long[] degree = new long[str.length()];
 
@@ -23,33 +19,54 @@ public class zFunction {
             degree[i] = (degree[i - 1] * x) % remains;
             array[i] = (array[i - 1] * x + (long) (str.charAt(i))) % remains;
         }
-        int[] answer = new int[str.length()];
-        pw.print("0 ");
+
+        pw.print("0 "); //Сохраняю все сразу в буфер обмена
         for (int i = 1; i <= str.length() - 1; i++) {
             int suffixBeg = i;
-            int length = 1;
+            int suffixLen = str.length()-i;
+            int length;
             int prefix = 0;
-            boolean flag= true;
-            for (int j = i; j<=str.length()-1;j++) {
-                boolean helper = (array[prefix] + array[suffixBeg-1] * degree[length]) % remains == array[j] % remains;
-                if (helper) {
-                    prefix++;
-                    length++;
-                    flag = false;
-                    continue;
-                }
-                break;
-            }
-            if(flag) {
+            //проверяю совпадения первых символов, если они не совпадают, то очевидно, что суффикс не совпадает вообще
+            if((array[0]+array[suffixBeg-1]*degree[1])%remains!=array[suffixBeg]%remains) {
                 pw.print("0 ");
+            //проверяю, вдруг совпадает весь символ
+            } else if ((array[suffixLen-1] + array[suffixBeg-1] * degree[suffixLen]) % remains == array[str.length()-1] % remains) {
+                pw.print(suffixLen+" ");
+            //Бинарный поиск по индексу элемента
+            }else {
+                int left = i;
+                int right = str.length()-1;
+                while (left<right){
+                    int index = (left+right)/2;//предположительный индекс конца совпадения префикса с суффиксом
+                    int indexLen = index-suffixBeg+1;
+                    //проверка совпадения суффикса до index с префиксом
+                    boolean helper1 = (array[indexLen-1] + array[suffixBeg-1] * degree[indexLen])%remains == array[index]%remains;
+                    //проверка совпадение суффикса до index+1 c префиксом
+                    boolean helper2 = (array[indexLen] + array[suffixBeg-1] * degree[indexLen+1])%remains == array[index+1]%remains;
+                    //если суффикс до index совпадает с префиксом и суффикс до index+1 не совпадает, то длинна индекса это ответ
+                    if(helper1 && !helper2){
+                        pw.print(indexLen+" ");
+                        break;
+                    }
+                    else if(helper1){
+                        left = index;
+                        index = (left+right)/2;
+                        indexLen = index-suffixBeg+1;
+                    }
+                    else {
+                        right = index;
+                        index = (left+right)/2;
+                        indexLen = index - suffixBeg+1;
+                    }
+                }
             }
-            else
-                pw.print((length-1)+" ");
         }
 
-        pw.flush();
+        pw.flush();//вывод всего из буфера в консоль
 
     }
+
+
 }
 
 
