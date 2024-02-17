1b4c574da3a63a72612218e0c7e196792edf5e61
Muradly Muhammed <mmuradly2@gmail.com>
2024-02-17 13:47:45 +0300

    added Dijkstra With Ways

--- src/DijkstraWithWays.java
+++ src/DijkstraWithWays.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
-public class Dijkstra {
+
+public class DijkstraWithWays {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] mas = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
@@ -12,12 +13,14 @@ public class Dijkstra {
         int finish = mas[2];
         int[] distance = new int[N+1];//Чтобы индексы совпадали с вершинами графов
         boolean[] visited = new boolean[N+1];
+        int[] parents = new int[N+1];
 
         Arrays.fill(visited,false);
         visited[0] = true;
 
         Arrays.fill(distance, Integer.MAX_VALUE);//Заполнение всего массива
         distance[start] = 0;//Так как мы уже находимся в этой точке
+        parents[start] = -1;
 
         HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
 
@@ -42,17 +45,24 @@ public class Dijkstra {
             int cost = distance[now];
             for(Integer x: inner.keySet()){
                 int newCost = cost + inner.get(x);
-                if(distance[x]>newCost)//Меняем длину пути, только если она меньше
+                if(distance[x]>newCost) {//Меняем длину пути, только если она меньше
                     distance[x] = newCost;
+                    parents[x] = now;//Записываем откуда мы пришли в эту вершину
+                }
 
             }
         }
-        if(distance[finish]==Integer.MAX_VALUE){//Если там бесконечность, значит до вершины невозможно добраться
+        if(distance[finish] == Integer.MAX_VALUE){
             System.out.println(-1);
-        }else {
-            System.out.println(distance[finish]);
+            return;
         }
-
+        String answer = " " + finish;
+        int index = parents[finish];
+        while (index!=-1){
+            answer = " " + index + answer;
+            index = parents[index];
+        }
+        System.out.println(answer.substring(1,answer.length()));
 
     }
     //Функция для нахождения непосещенной вершины с минимальным расстоянием(Возвращает индекс)
